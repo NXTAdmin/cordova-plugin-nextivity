62a62a26f9ac8df0c48076e75800d2c0cdbdaecd
Mark Valecko <valeckom@nextivityinc.com>
2021-04-05 14:29:18 -0700

    update shell

--- src/ios/CDVTelephony.swift
+++ src/ios/CDVTelephony.swift
@@ -1,14 +1,18 @@
+/*
+* Notes: The @objc shows that this class & function should be exposed to Cordova.
+*/
 @objc(Telephony) class Telephony : CDVPlugin {
-  // MARK: Properties
-  var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-
-  @objc(getCellInfo:) func add(_ command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs:
-    "success")
-    }
-
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+  @objc(getCellInfo:) // Declare your function name.
+  func getCellInfo(command: CDVInvokedUrlCommand) { // write the function code.
+    /*
+     * Always assume that the plugin will fail.
+     * Even if in this example, it can't.
+     */
+    // Set the plugin result to fail.
+    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+    // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+    // Send the function result back to Cordova.
+    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
 }
