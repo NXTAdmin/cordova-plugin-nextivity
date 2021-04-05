c3429278972fa3a0b4ddea1e906261164fe0421d
Mark Valecko <valeckom@nextivityinc.com>
2021-04-05 14:12:35 -0700

    quotes

--- src/ios/CDVTelephony.swift
+++ src/ios/CDVTelephony.swift
@@ -5,7 +5,8 @@
   @objc(getCellInfo:) func add(_ command: CDVInvokedUrlCommand) {
     var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
 
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: 'success')
+    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs:
+    "success")
     }
 
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
