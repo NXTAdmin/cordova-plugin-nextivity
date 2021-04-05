0b549a5ab4e2998cda8f0abffe1a4f2713cb3e96
Mark Valecko <valeckom@nextivityinc.com>
2021-04-05 15:40:12 -0700

    get obj

--- src/ios/CDVTelephony.swift
+++ src/ios/CDVTelephony.swift
@@ -2,23 +2,10 @@
 * Notes: The @objc shows that this class & function should be exposed to Cordova.
 */
 @objc(Telephony) class Telephony : CDVPlugin {
-
-var a = new CTTelephonyNetworkInfo();
-    var b = a.serviceCurrentRadioAccessTechnology
-
-    typealias JSONDictionary = [String : Any]
-
-    func asString(jsonDictionary: JSONDictionary) -> String {
-      do {
-        let data = try JSONSerialization.data(withJSONObject: jsonDictionary, options: .prettyPrinted)
-        return String(data: data, encoding: String.Encoding.utf8) ?? ""
-      } catch {
-        return ""
-      }
-    }
-
   @objc(getCellInfo:) // Declare your function name.
   func getCellInfo(command: CDVInvokedUrlCommand) { // write the function code.
+    var a = new CTTelephonyNetworkInfo();
+    var b = a.serviceCurrentRadioAccessTechnology
     /*
      * Always assume that the plugin will fail.
      * Even if in this example, it can't.
@@ -26,7 +13,8 @@ var a = new CTTelephonyNetworkInfo();
     // Set the plugin result to fail.
     var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
     // Set the plugin result to succeed.
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: asString(b));
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,
+        messageAs: b;
     // Send the function result back to Cordova.
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
