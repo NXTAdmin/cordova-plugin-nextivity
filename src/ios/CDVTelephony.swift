347ca58d96a0ec709ac29fba8c5f2ce573677e58
Mark Valecko <valeckom@nextivityinc.com>
2021-04-05 13:04:13 -0700

    add ios shell plugin

---
+++ src/ios/CDVTelephony.swift
@@ -0,0 +1,13 @@
+@objc(Telephony) class Telephony : CDVPlugin {
+  // MARK: Properties
+  var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+
+  @objc(getCellInfo:) func add(_ command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+
+    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: 'success')
+    }
+
+    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+  }
+}
