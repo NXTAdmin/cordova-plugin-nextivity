58710a907f4345fd88d0c89ac43cdc0eca44fd6f
Mark Valecko <valeckom@nextivityinc.com>
2021-08-03 10:16:56 -0700

    populate getCarrierInfo data

--- src/ios/TelephonyPlugin.m
+++ src/ios/TelephonyPlugin.m
@@ -1,33 +1,48 @@
 #import "TelephonyPlugin.h"
-//#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
 
 @implementation Telephony
 
 - (void)getCarrierInfo:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult;
-
-//    CTTelephonyNetworkInfo* networkInfo = [CTTelephonyNetworkInfo new];
-//    CTCarrier* carrier = networkInfo.subscriberCellularProvider;
-
-        NSDictionary *data = @{
-                @"carrierName" : @"",
-                @"isoCountryCode" : @"",
-                @"mobileCountryCode" : @"",
-                @"mobileNetworkCode" : @"",
+    // Run in background thread
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult;
+        CTCarrier* carrier;
+        CTTelephonyNetworkInfo* networkInfo = [CTTelephonyNetworkInfo new];
+        NSDictionary* data = @{
+            @"carrierName" : @"",
+            @"isoCountryCode" : @"",
+            @"mobileCountryCode" : @"",
+            @"mobileNetworkCode" : @"",
         };
 
-//    NSDictionary *data = @{
-//            carrierName : carrier?.carrierName ?? "",
-//            isoCountryCode : carrier?.isoCountryCode ?? "",
-//            mobileCountryCode : carrier?.mobileCountryCode ?? "",
-//            mobileNetworkCode : carrier?.mobileNetworkCode ?? "",
-//
-//    }
+        if (@available(iOS 12.0, *)) {
+            // Find the active provider
+            for (NSString* key in networkInfo.serviceSubscriberCellularProviders){
+                if (networkInfo.serviceSubscriberCellularProviders[key] != nil){
+                    carrier = networkInfo.serviceSubscriberCellularProviders[key];
+                    break;
+                }
+            }
+        } else {
+            carrier = networkInfo.subscriberCellularProvider;
+        };
+
+        if (carrier){
+            data = @{
+                @"carrierName" : carrier.carrierName ? carrier.carrierName : @"",
+                @"isoCountryCode" : carrier.isoCountryCode ? carrier.isoCountryCode : @"",
+                @"mobileCountryCode" : carrier.mobileCountryCode ? carrier.mobileCountryCode : @"",
+                @"mobileNetworkCode" : carrier.mobileNetworkCode ? carrier.mobileNetworkCode : @"",
+            };
+        }
 
 
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:data];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:data];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 @end
